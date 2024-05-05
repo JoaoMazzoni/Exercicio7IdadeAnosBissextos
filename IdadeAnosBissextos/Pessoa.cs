287fb2664a34a49d1ef49ae65187429abdc0d991
JoaoMazzoni <joao.mazzoni@5by5.com>
2024-05-05 14:58:55 -0300

    Add project files.

---
+++ IdadeAnosBissextos/Pessoa.cs
@@ -0,0 +1,75 @@
+﻿
+namespace IdadeAnosBissextos
+{
+    internal class Pessoa
+    {
+        string nome;
+        string profissao;
+        int idade;
+
+        public Pessoa(string nome, string profissao, int idade)
+        {
+            this.nome = nome;
+            this.profissao = profissao;
+            this.idade = idade;
+        }
+
+
+        public void setNome(string nome)
+        {
+            this.nome = nome;
+        }
+
+        public string getNome()
+        {
+            return this.nome;
+        }
+
+        public void setProfissao(string profissao)
+        {
+            this.profissao = profissao;
+        }
+
+        public string getProfissao()
+        {
+            return this.profissao;
+        }
+
+        public void setIdade(int idade)
+        {
+            this.idade = idade;
+        }
+
+        public int getIdade()
+        {
+            return this.idade;
+        }
+
+        public void idadeBissexto()
+        {
+            DateTime dataAtual;
+            dataAtual = DateTime.Now;
+            int anoAtual = dataAtual.Year;
+            int idade = this.getIdade();
+            int anoNascimento;
+
+            Console.WriteLine("\nO ano atual é: " + anoAtual);
+
+            anoNascimento = anoAtual - idade;
+            Console.WriteLine($"\nO ano de nascimento da pessoa é: {anoNascimento}");
+
+            //Calcular idade em anos bissextos
+            float IdadeEmBissextos = ((anoAtual - anoNascimento) + 1) / 4;
+            Console.WriteLine("\nA idade da pessoa em anos bissextos é: {0} anos", IdadeEmBissextos);
+
+        }
+
+        public override string? ToString()
+        {
+            return $"\nNome: {this.nome}\nProfissão: {this.profissao}\nIdade: {this.idade} anos";
+        }
+    
+    }
+
+
+}
---
+++ IdadeAnosBissextos/Program.cs
@@ -0,0 +1,40 @@
+﻿using IdadeAnosBissextos;
+using System.Runtime.CompilerServices;
+
+
+public class Program
+{
+
+    internal static void Main(string[] args)
+    {
+        while (true)
+        {
+            Pessoa pessoa = criarPessoa();
+            Console.WriteLine(pessoa.ToString());
+            Console.ReadLine();
+            pessoa.idadeBissexto();
+            Console.ReadLine();
+            Console.Clear();
+        }
+    }
+
+    static Pessoa criarPessoa()
+    {
+        string nome;
+        string profissao;
+        int idade;
+        Console.Write("\nInforme o nome: ");
+        nome = Console.ReadLine();
+        Console.Write("\nInforme a profissão: ");
+        profissao = Console.ReadLine();
+        Console.Write("\nInforme a idade que foi ou será completada no ano atual: ");
+        idade = int.Parse(Console.ReadLine());
+
+        return new Pessoa(nome, profissao, idade);
+
+
+    }
+
+
+    
+}
